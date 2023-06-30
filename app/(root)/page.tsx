c794b513f7111c93ce0894b2e10cfb33c5321684
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-06-30 21:18:06 +0530

    clerk init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <main className="p-5">
+      <h1>ABhishek</h1>
+      <Button>ABhishek</Button>
+    </main>
+  );
+}
