12a4b675db44a0dea9b33c3702044ce75e8ee82b
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-08-13 01:06:24 +0530

    create store api

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,10 +1,53 @@
 'use client';
 
-import { useStoreModal } from '@/hooks/use-store-modal';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import { Modal } from '@/components/ui/modal';
+import { useStoreModal } from '@/hooks/use-store-modal';
+
+const formSchema = z.object({
+  name: z.string().min(3),
+});
 
 export const StoreModal = () => {
+  const [loading, setLoading] = useState(false);
+
   const modalStore = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    try {
+      setLoading(true);
+
+      const res = await axios.post('/api/stores', values);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Modal
@@ -13,7 +56,43 @@ export const StoreModal = () => {
       isOpen={modalStore.isOpen}
       onClose={modalStore.onClose}
     >
-      Create Store
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        placeholder="E-commerce"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex justify-end items-center">
+                <Button
+                  disabled={loading}
+                  variant="outline"
+                  onClick={modalStore.onClose}
+                >
+                  Cancle
+                </Button>
+                <Button disabled={loading} type="submit">
+                  Continue
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   );
 };
