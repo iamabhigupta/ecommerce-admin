ceb1fc85147c78073bd10bff45d0202af8e539ee
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-08-15 20:25:55 +0530

    data table done

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,13 +1,19 @@
 'use client';
 
 import { Plus } from 'lucide-react';
-
-import Heading from '@/components/ui/heading';
-import { Button } from '@/components/ui/button';
-import { Separator } from '@/components/ui/separator';
 import { useParams, useRouter } from 'next/navigation';
 
-const BillboardClient = () => {
+import { Button } from '@/components/ui/button';
+import Heading from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
+import { BillboardColumns, columns } from './columns';
+import { DataTable } from '@/components/ui/data-table';
+
+interface Props {
+  data: BillboardColumns[];
+}
+
+const BillboardClient: React.FC<Props> = ({ data }) => {
   const params = useParams();
   const router = useRouter();
 
@@ -15,7 +21,7 @@ const BillboardClient = () => {
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title="Billboards (0)"
+          title={`Billboards (${data.length})`}
           description="Manage billboards for your store"
         />
         <Button
@@ -26,6 +32,7 @@ const BillboardClient = () => {
         </Button>
       </div>
       <Separator />
+      <DataTable columns={columns} data={data} searchKey="label" />
     </>
   );
 };
---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { ColumnDef } from '@tanstack/react-table';
+
+export type BillboardColumns = {
+  id: string;
+  label: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<BillboardColumns>[] = [
+  {
+    accessorKey: 'label',
+    header: 'Label',
+  },
+  {
+    accessorKey: 'createdAt',
+    header: 'Date ',
+  },
+];
