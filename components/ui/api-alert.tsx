1d6321e689deaaac96357632193cd26830289e4f
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-08-15 21:20:37 +0530

    billboard done

--- components/ui/api-alert.tsx
+++ components/ui/api-alert.tsx
@@ -1,37 +1,48 @@
-'use client';
-
 import { Copy, Server } from 'lucide-react';
 import { toast } from 'react-hot-toast';
 
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import { Badge } from '@/components/ui/badge';
+import { Badge, BadgeProps } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 
-interface Props {
+interface ApiAlertProps {
   title: string;
   description: string;
-  variant: 'Public' | 'Admin';
+  variant: 'public' | 'admin';
 }
 
-const ApiAlert: React.FC<Props> = ({ title, description, variant }) => {
-  const onCopy = () => {
+const textMap: Record<ApiAlertProps['variant'], string> = {
+  public: 'Public',
+  admin: 'Admin',
+};
+
+const variantMap: Record<ApiAlertProps['variant'], BadgeProps['variant']> = {
+  public: 'secondary',
+  admin: 'destructive',
+};
+
+const ApiAlert: React.FC<ApiAlertProps> = ({
+  title,
+  description,
+  variant = 'public',
+}) => {
+  const onCopy = (description: string) => {
     navigator.clipboard.writeText(description);
-    toast.success('API route copied to the clipboard');
+    toast.success('API Route copied to clipboard.');
   };
+
   return (
     <Alert>
-      <Server className="h-4 w-4 mt-[3px]" />
+      <Server className="h-4 w-4 pt-[3px]" />
       <AlertTitle className="flex items-center gap-x-2">
         {title}
-        <Badge variant={variant === 'Admin' ? 'destructive' : 'secondary'}>
-          {variant}
-        </Badge>
+        <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
       </AlertTitle>
       <AlertDescription className="mt-4 flex items-center justify-between">
         <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
           {description}
         </code>
-        <Button variant="outline" size="sm" onClick={onCopy}>
+        <Button variant="outline" size="sm" onClick={() => onCopy(description)}>
           <Copy className="h-4 w-4" />
         </Button>
       </AlertDescription>
